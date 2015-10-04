4c6bc4ec2fcd6c568cb8a3674bb0b4d989c20b9a
Kelly Sutton <michael.k.sutton@gmail.com>
2015-10-04 14:13:52 -0700

    Have the binary operate on stdin, not globbing for files

--- bin/write-good.js
+++ bin/write-good.js
@@ -2,22 +2,14 @@
 
 var fs        = require('fs');
 var writeGood = require('../write-good');
-var annotate  = require('../lib/annotate');
 
 var args      = process.argv.slice(2);
-var files     = args.filter(function (arg) {
-  return arg.substr(0, 2) !== '--';
-});
+var inputData = null;
 
 if (args[0] === '--version'){
   var version = require('../package.json').version;
   console.log('write-good version ' + version);
   process.exit(0);
-}
-
-if (files.length === 0) {
-  console.log('You did not provide any files to check');
-  process.exit(1);
 }
 
 var opts      = {
@@ -52,13 +44,35 @@ Object.keys(opts).forEach(function (name) {
   }
 });
 
-var exitCode = 0;
-files.forEach(function (file) {
-  var contents = fs.readFileSync(file, 'utf8');
-  var suggestions = writeGood(contents, opts);
+var BUFSIZE = 256;
+var buffer = new Buffer(BUFSIZE);
+var bytesRead;
+var result = "";
 
-  exitCode += suggestions.length;
-  console.log(JSON.stringify({ "suggestions": suggestions }));
-});
+while (true) {
+  bytesRead = 0;
 
-process.exit(exitCode);
+  try {
+    bytesRead = fs.readSync(process.stdin.fd, buffer, 0, BUFSIZE);
+  } catch (e) {
+      if (e.code === "EAGAIN") {
+        console.error("ERROR: interactive stdin input not supported");
+        process.exit(1);
+      } else if (e.code === "EOF") {
+        break;
+      }
+      throw e;
+  }
+
+  // No more stdin input available
+  if (bytesRead === 0) {
+    break;
+  }
+
+  result += buffer.toString(null, 0, bytesRead);
+}
+
+var suggestions = writeGood(result, opts);
+console.log(JSON.stringify({ "suggestions": suggestions }));
+
+process.exit(0);
